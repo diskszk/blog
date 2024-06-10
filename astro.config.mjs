01f022602917579a9a8ebb07270ffb204b852ebb
diskszk <ibalucky23@gmail.com>
2024-06-11 01:27:38 +0900

    feat: 開発サーバーをlocalhost:3000に変更する

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
