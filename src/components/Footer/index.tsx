5ebac18522d54782ad889990687e4cc138fb3714
diskszk <ibalucky23@gmail.com>
2024-06-12 14:25:12 +0900

    feat: Footerコンポーネントを作成する

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@radix-ui/themes";
+import { border } from "./styles.css";
+
+export const Footer: React.FC = () => (
+  <footer>
+    <Flex align="center" className={border} height="64px" px="16px">
+      <Text color="gray">
+        <small>© 2024 diskszk </small>
+      </Text>
+    </Flex>
+  </footer>
+);
