2f7999158a8850e01e77e150ab0ab1da2d30c8f1
diskszk <ibalucky23@gmail.com>
2024-06-13 21:13:46 +0900

    feat: レスポンシブデザインを導入する

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,4 +1,17 @@
-export const styles = {
-  headerHeight: "112px",
-  footerHeight: "64px",
+type Style = {
+  [key: string]: {
+    pc: string;
+    sp: string;
+  };
 };
+
+export const styles = {
+  headerHeight: {
+    pc: "112px",
+    sp: "64px",
+  },
+  footerHeight: {
+    pc: "64px",
+    sp: "32px",
+  },
+} satisfies Style;
