60435c7a3bd4732b73d7f30431fb037546b2161a
diskszk <ibalucky23@gmail.com>
2024-06-16 22:12:51 +0900

    feat: 記事一覧を日付順でソートして表示する

--- src/partials/BlogList/index.stories.tsx
+++ src/partials/BlogList/index.stories.tsx
@@ -18,6 +18,7 @@ export const Default: Story = {
         data: {
           title: "ポケモンダブルバトル必勝法",
           tags: ["React", "vite", "css", "apple", "pokemon", "ポケモンSV"],
+          updated_at: "2021-08-01T09:00:00",
         },
       },
       {
@@ -25,6 +26,7 @@ export const Default: Story = {
         data: {
           title: "必ず勝てるポケモンバトル",
           tags: ["TypeScript", "react"],
+          updated_at: "2021-07-21T09:00:00",
         },
       },
       {
@@ -32,6 +34,7 @@ export const Default: Story = {
         data: {
           title: "ダブルバトルの覇者になる",
           tags: ["CSS", "TypeScript"],
+          updated_at: "2021-06-15T09:00:00",
         },
       },
       {
@@ -39,6 +42,7 @@ export const Default: Story = {
         data: {
           title: "強いポケモン100選",
           tags: ["Astro", "TypeScript"],
+          updated_at: "2023-08-21T09:00:00",
         },
       },
       {
@@ -46,6 +50,7 @@ export const Default: Story = {
         data: {
           title: "サポートポケモンの選び方",
           tags: ["AWS", "TypeScript"],
+          updated_at: "2020-01-21T09:00:00",
         },
       },
       {
@@ -53,6 +58,7 @@ export const Default: Story = {
         data: {
           title: "まもるを使うタイミングが全て",
           tags: ["node.js", "TypeScript"],
+          updated_at: "2024-06-16T09:00:00",
         },
       },
       {
@@ -60,6 +66,7 @@ export const Default: Story = {
         data: {
           title: "全体わざと単体技どちらを選ぶか",
           tags: ["Nest.js", "TypeScript"],
+          updated_at: "2021-08-21T09:00:00",
         },
       },
     ],
