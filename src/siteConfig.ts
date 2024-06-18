843077295791b3616c2f36a8ed7637dd52bdae14
diskszk <ibalucky23@gmail.com>
2024-06-18 14:52:49 +0900

    feat: titleタグにサイト名が入るようにする

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  siteName: "Tech Blog.",
+  siteName: "うれたのTech Blog.",
   githubUrl: "https://github.com/diskszk",
   repoURL: "https://github.com/diskszk/blog",
   userIcon: "/assets/images/icon.jpeg",
